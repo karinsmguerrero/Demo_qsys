f248cdff8a121653c4fafaefa69345d3607d8e70
Karina Martinez <karinamguerrero@estudiantec.cr>
2025-06-10 21:43:56 -0600

    [Update] change connections

--- reloj_soc/reloj_soc_bb.v
+++ reloj_soc/reloj_soc_bb.v
@@ -9,6 +9,7 @@ module reloj_soc (
 	buttons_export,
 	clk_clk,
 	leds_export,
+	reset_reset_n,
 	memory_mem_a,
 	memory_mem_ba,
 	memory_mem_ck,
@@ -24,8 +25,7 @@ module reloj_soc (
 	memory_mem_dqs_n,
 	memory_mem_odt,
 	memory_mem_dm,
-	memory_oct_rzqin,
-	reset_reset_n);	
+	memory_oct_rzqin);	
 
 	input		audio_0_external_interface_BCLK;
 	output		audio_0_external_interface_DACDAT;
@@ -36,6 +36,7 @@ module reloj_soc (
 	input	[31:0]	buttons_export;
 	input		clk_clk;
 	output	[31:0]	leds_export;
+	input		reset_reset_n;
 	output	[12:0]	memory_mem_a;
 	output	[2:0]	memory_mem_ba;
 	output		memory_mem_ck;
@@ -52,5 +53,4 @@ module reloj_soc (
 	output		memory_mem_odt;
 	output		memory_mem_dm;
 	input		memory_oct_rzqin;
-	input		reset_reset_n;
 endmodule
